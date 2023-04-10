8d87396b56234b793fb7a7f9ebb7fc9dd84a507f
tzoug <dylan.apardian@gmail.com>
2023-04-10 13:40:38 -0400

    Implemented settings - Option to enable/disable surrounding FLOAT variables with single quotes (if number is too big) - Option to replace the variables in the query with their corresponding values

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,50 @@
+import * as vscode from "vscode";
+
 function normalizeQuery(query: string) {
   return query.replace(/''/g, "'");
+}
+
+function doesParamFloatExistInParamDeclaration(parameterDeclaration:string, paramName:string){  
+  let paramWithUpperType = paramName + " FLOAT";
+  let paramWithLowerType = paramName + " float";
+  let indexOfUpper = parameterDeclaration.indexOf(paramWithUpperType);
+  let indexOfLower = parameterDeclaration.indexOf(paramWithLowerType);
+
+  if (indexOfUpper != -1 || indexOfLower != -1){
+    return true;
+  }
+
+  return false;
+}
+
+function isParamSurroundedWithSingleQuotes(paramValue:string){
+  if (paramValue[0] != "'" && paramValue[paramValue.length] != "'"){
+    return true;
+  }
+
+  return false;
+}
+
+function surroundFloatWithQuotesFlow(parameterDeclaration:string, paramName:string, paramValue:string, value:string, paramDict:Map<string, string>){
+  if (doesParamFloatExistInParamDeclaration(parameterDeclaration, paramName)){
+    if (isParamSurroundedWithSingleQuotes(paramValue)){
+        paramValue = "'" + paramValue + "'";
+        paramDict.set(paramName, paramValue);
+
+        value = paramName + "=" + paramValue;
+        return value;
+    }
+  }
+
+  return value;
+}
+
+function replaceParamInQueryFlow(paramDict:Map<string, string>, normalizedQuery:string){
+  paramDict.forEach((value:string, key:string) => {    
+    normalizedQuery = normalizedQuery.split(key).join(value);
+  })
+
+  return normalizedQuery;
 }
 
 function parseQuery(
@@ -8,7 +53,11 @@ function parseQuery(
   query: string
 ) {
   let parameters = parameterDeclaration.split(",");
-  let result = "DECLARE " + parameterDeclaration + "\n";
+  let result = "DECLARE " + parameterDeclaration + "\n";  
+
+  let paramDict = new Map<string, string>();
+  const isSurroundFloatWithSingleQuotesEnabled = vscode.workspace.getConfiguration().get('spexecutesql.surroundFloatWithSingleQuotes');
+  const isInlineParamEnabled = vscode.workspace.getConfiguration().get('spexecutesql.replaceVariablesInQuery');;
 
   parameterValue.split(",").forEach((value, index) => {
     value = value.trim();
@@ -16,10 +65,25 @@ function parseQuery(
     if (value[0] !== "@") {
       value = parameters[index].split(" ")[0] + "=" + value;
     }
+
+    let paramName = value.split("=")[0];
+    let paramValue = value.split("=")[1];  
+    paramDict.set(paramName, paramValue);   
+    
+    if (isSurroundFloatWithSingleQuotesEnabled){
+      value = surroundFloatWithQuotesFlow(parameterDeclaration, paramName, paramValue, value, paramDict);
+    }
+
     result += "SET " + value + "\n";
   });
 
-  result += "\n" + normalizeQuery(query) + "\n";
+  let normalizedQuery = normalizeQuery(query);
+  
+  if (isInlineParamEnabled){
+    normalizedQuery = replaceParamInQueryFlow(paramDict, normalizedQuery);
+  }
+
+  result += "\n" + normalizedQuery + "\n";
 
   return result;
 }
